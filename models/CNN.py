67672d06638b5134957e49198a1651aef129f564
chekirou <chekirouhakim@gmail.com>
2020-02-20 17:39:48 +0100

    resize

--- models/CNN.py
+++ models/CNN.py
@@ -43,3 +43,20 @@ class CNN(nn.Module):
         output = self.conv3(output)
         output = output.view(output.shape[0],-1)
         return self.classifier(output)
+    def train():
+      self.train()
+      for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = network(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+          print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+            epoch, batch_idx * len(data), len(train_loader.dataset),
+            100. * batch_idx / len(train_loader), loss.item()))
+          train_losses.append(loss.item())
+          train_counter.append(
+            (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+          torch.save(network.state_dict(), '/results/model.pth')
+          torch.save(optimizer.state_dict(), '/results/optimizer.pth')
